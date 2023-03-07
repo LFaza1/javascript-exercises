83260f04b2fd4381c9d40c9d12bd7413f255705d
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-03-08 00:35:18 +0530

    Done 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    for(let i = string.length; i > 0; i--){
+        reversed += string.slice(i-1, i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
